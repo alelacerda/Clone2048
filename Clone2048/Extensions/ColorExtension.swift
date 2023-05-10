0a07d8d419374720463670a2cc4919f7019e50b7
Alessandra F. Lacerda <flacerdaale@gmail.com>
2023-05-10 19:58:15 -0300

    Adds colors

---
+++ Clone2048/Extensions/ColorExtension.swift
@@ -0,0 +1,28 @@
+//
+//  Colors.swift
+//  Clone2048
+//
+//  Created by Alessandra Fernandes Lacerda on 10/05/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let gameBackground = Color("gameBackground")
+    static let boardBackground = Color("boardBackground")
+    static let lightText = Color("lightText")
+    static let darkText = Color("darkText")
+    static let tile2 = Color("tile2")
+    static let tile4 = Color("tile4")
+    static let tile8 = Color("tile8")
+    static let tile16 = Color("tile16")
+    static let tile32 = Color("tile32")
+    static let tile64 = Color("tile64")
+    static let tile128 = Color("tile128")
+    static let tile256 = Color("tile256")
+    static let tile512 = Color("tile512")
+    static let tile1024 = Color("tile1024")
+    static let tile2048 = Color("tile2048")
+    static let tileMax = Color("tileMax")
+    static let tileEmpty = Color("tileEmpty")
+}
