c4784158c1c213695a1379fe58ec7e34864daff1
Alessandra F. Lacerda <flacerdaale@gmail.com>
2023-05-10 14:38:27 -0300

    Initial Commit

---
+++ Clone2048/Clone2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Clone2048App.swift
+//  Clone2048
+//
+//  Created by Alessandra Fernandes Lacerda on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Clone2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
