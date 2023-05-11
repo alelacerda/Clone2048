5fd447b7d16e47794fa64e95c09667787992df20
Alessandra F. Lacerda <flacerdaale@gmail.com>
2023-05-11 16:34:45 -0300

    Adds drag gesture recognition

--- Clone2048/Views/GameView.swift
+++ Clone2048/Views/GameView.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
+enum Direction{
+    case up, down, left, right, none
+}
+
 struct GameView: View {
     var board = [[0,0,0,4], [0,0,2,16], [2,16,32,4], [32,128,64,8]]
     
+    // Drag Gesture Variables
+    @State var direction: Direction = .none
+    let detectDirectionalDrags = DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
+
     var body: some View {
         VStack {
             HeaderView()
@@ -18,6 +26,31 @@ struct GameView: View {
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.gameBackground)
+        .gesture(DragGesture()
+            .onEnded { value in
+                print(value.translation)
+                
+                if value.translation.width < 0 && value.translation.height > -30 && value.translation.height < 30 {
+                    self.direction = .left
+                    print("left swipe")
+                }
+                else if value.translation.width > 0 && value.translation.height > -30 && value.translation.height < 30 {
+                    self.direction = .right
+                    print("right swipe")
+                }
+                else if value.translation.height < 0 && value.translation.width < 100 && value.translation.width > -100 {
+                    self.direction = .up
+                    print("up swipe")
+                }
+                else if value.translation.height > 0 && value.translation.width < 100 && value.translation.width > -100 {
+                    self.direction = .down
+                    print("down swipe")
+                }
+                else {
+                    self.direction = .none
+                    print("no clue")
+                }
+            })
     }
 }
 
